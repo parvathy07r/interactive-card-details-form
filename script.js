a413a5f298f2b1adb0a0b997e27b791ff6261408
Parvathy R <parvathy07r@gmail.com>
2024-07-18 19:41:42 +0530

    Added code to update card details real time

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
 const formData = document.querySelector("#form_data");
 const displayNumber = document.querySelector(".display-number");
 const displayName = document.querySelector(".display-name");
-const displayExpiry = document.querySelector(".display-expiry");
+const displayMonth = document.querySelector(".display-month");
+const displayYear = document.querySelector(".display-year");
 const displayCVC = document.querySelector(".display-cvc");
 const error = document.querySelector(".error");
+const nameInput = document.querySelector("#name");
+const numberInput = document.querySelector("#number");
+const monthInput = document.querySelector("#month");
+const yearInput = document.querySelector("#year");
+const cvcInput = document.querySelector("#cvc");
 
-formData.addEventListener("submit", function(event) {
-    
+function setValue(element, value){
+    element.innerText = value
+}
+
+nameInput.addEventListener("input", function(event){
+    setValue(displayName, event.target.value);
+});
+
+numberInput.addEventListener("input", function (event) {
+    displayNumber.innerText = event.target.value;
+});
+
+monthInput.addEventListener("input", function (event) {
+    displayMonth.innerText = event.target.value;
+});
+
+yearInput.addEventListener("input", function (event) {
+    displayYear.innerText = event.target.value;
+});
+
+cvcInput.addEventListener("input", function (event) {
+    displayCVC.innerText = event.target.value;
+});
+
+formData.addEventListener("submit", function (event) {
     event.preventDefault();
 
     const form = event.target;
     const cardName = form["name"].value;
     const cardNumber = form["number"].value;
     const expiryDate = form["dd"].value;
-    const expiryMonth = form["mm"].value;    
+    const expiryMonth = form["mm"].value;
     const cvc = form["cvc"].value;
 
-    displayCVC.innerHTML = "";
-    displayExpiry.innerHTML = "";
-    displayName.innerHTML = "";
-    displayNumber.innerHTML = "";
-    error.innerHTML = "";
-    
-    if(isNaN(cardNumber)){
-        error.append(enterDetails("error"));
-    }else {
-        displayNumber.append(enterDetails(cardNumber));
-    }
-
-    if(cardName) {
-        displayName.append(enterDetails(cardName));
-    }
-
-    if(expiryDate && expiryMonth) {
-        displayExpiry.append(enterDetails(`${expiryDate} ${expiryMonth}`));
-    }
-
-    if(cvc) {
-        displayCVC.append(enterDetails(cvc));
-    }
+    // Validations
 
 });
 
-function enterDetails(value) {
-    const node = document.createElement("p");
-    node.innerHTML = value;
-    return node;
-}
 
