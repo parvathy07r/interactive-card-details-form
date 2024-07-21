171bc9a9523f59a8eaab003ecc4b89ea4c4acbfb
Parvathy R <parvathy07r@gmail.com>
2024-07-21 12:58:35 +0530

    updated styles

--- script.js
+++ script.js
@@ -25,118 +25,136 @@ let outcome;
 
 
 function setValue(element, value) {
-    element.innerText = value
+
+  nameInput.style.border = "1px solid black";
+    numberInput.style.border = "1px solid black";
+    monthInput.style.border = "1px solid black";
+    yearInput.style.border = "1px solid black";
+    cvcInput.style.border = "1px solid black";
+  nameError.innerHTML = "";
+  numberError.innerHTML = "";
+  dateError.innerHTML = "";
+  cvcError.innerHTML = "";
+  element.innerText = value
 }
 
 //add eventlisteners to the input elements to display card detils real time
 nameInput.addEventListener("input", function (event) {
-    setValue(displayName, event.target.value);
+  setValue(displayName, event.target.value);
 });
 
 numberInput.addEventListener("input", function (event) {
-	setValue(displayNumber, event.target.value);
+  setValue(displayNumber, event.target.value);
 });
 
 monthInput.addEventListener("input", function (event) {
-	setValue(displayMonth, event.target.value);
+  setValue(displayMonth, event.target.value);
 });
 
 yearInput.addEventListener("input", function (event) {
-	setValue(displayYear, event.target.value);
+  setValue(displayYear, event.target.value);
 });
 
 cvcInput.addEventListener("input", function (event) {
-	setValue(displayCVC, event.target.value);
+  setValue(displayCVC, event.target.value);
 });
 
 //form submission handler
 formData.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    //extracting input values from the form
-    const form = event.target;
-    const cardName = form["name"].value;
-    const cardNumber = form["number"].value;
-    const expiryYear = form["year"].value;
-    const expiryMonth = form["month"].value;
-    const cvc = form["cvc"].value;
+  //extracting input values from the form
+  const form = event.target;
+  const cardName = form["name"].value;
+  const cardNumber = form["number"].value;
+  const expiryYear = form["year"].value;
+  const expiryMonth = form["month"].value;
+  const cvc = form["cvc"].value;
 
-    //calling function  for validating card details
-		const result = validation(cardName, cardNumber, expiryMonth, expiryYear, cvc);
+  //calling function  for validating card details
+  const result = validation(cardName, cardNumber, expiryMonth, expiryYear, cvc);
 
-    //redirect to next page the validations are true
-		if(result) {
-			redirect();
-		}
+  //redirect to next page the validations are true
+  if (result) {
+    redirect();
+  }
 
-    if(!result) {
-      nameInput.style.border = "1px solid red";
-      numberInput.style.border = "1px solid red";
-      monthInput.style.border = "1px solid red";
-      yearInput.style.border = "1px solid red";
-      cvcInput.style.border = "1px solid red";
-    }
-
+  if (!result) {
+   
+   
+   
+    
+    
+  }
 });
 
 //function to validate cardname, cardnumber,expirymonth, expiryyear, cvc
 function validation(cardName, cardNumber, expiryMonth, expiryYear, cvc) {
 
-	let isValid =true;
+  let isValid = true;
 
-    //validations
-		if(!cardName) {
-			setBlankError("can't be blank", nameError);
-			isValid = false;
-		}
+  //validations
+  if (!cardName) {
+    setBlankError("can't be blank", nameError);
+    isValid = false;
+    nameInput.style.border = "1px solid red";
+  }
 
-		if(!cardNumber) {
-			setBlankError("can't be blank", numberError);
-			isValid = false;
-		}
-    
-    if(cardNumber) {
-      setWrongFormatError(/^\d{16}$/, cardNumber, numberError, "wrong format, numbers only");
-      if(!outcome) {
-        isValid =false;
-      }
+  if (!cardNumber) {
+    setBlankError("can't be blank", numberError);
+    isValid = false;
+    numberInput.style.border = "1px solid red";
+  }
+
+  if (cardNumber) {
+    setWrongFormatError(/^\d{16}$/, cardNumber, numberError, "wrong format, numbers only");
+    if (!outcome) {
+      isValid = false;
+      numberInput.style.border = "1px solid red";
     }
+  }
 
-		if(!expiryMonth) {
-			setBlankError("can't be blank", dateError);
-			isValid = false;
-		}
+  if (!expiryMonth) {
+    setBlankError("can't be blank", dateError);
+    isValid = false;
+    monthInput.style.border = "1px solid red";
+  }
 
-    if(expiryMonth) {
-      setWrongFormatError(/^(0[1-9]|1[0-2])$/, expiryMonth, dateError, "wrong format, numbers only", isValid);
-      if(!outcome) {
-        isValid =false;
-      }
+  if (expiryMonth) {
+    setWrongFormatError(/^(0[1-9]|1[0-2])$/, expiryMonth, dateError, "wrong format, numbers only", isValid);
+    if (!outcome) {
+      isValid = false;
+      monthInput.style.border = "1px solid red";
     }
+  }
 
-		if(!expiryYear) {
-			setBlankError("can't be blank", dateError);
-			isValid = false;
-		}
-    
-    if(expiryYear) {
-      setWrongFormatError(/^\d{2}$/, expiryYear,dateError, "wrong format, numbers only", isValid);
-      if(!outcome) {
-        isValid =false;
-      }
+  if (!expiryYear) {
+    setBlankError("can't be blank", dateError);
+    isValid = false;
+    yearInput.style.border = "1px solid red";
+  }
+
+  if (expiryYear) {
+    setWrongFormatError(/^\d{2}$/, expiryYear, dateError, "wrong format, numbers only", isValid);
+    if (!outcome) {
+      isValid = false;
+      yearInput.style.border = "1px solid red";
     }
-      
-		if(!cvc) {
-			setBlankError("can't be blank", cvcError);
-			isValid = false;
-		}
-    
-    if(cvc) {
-      setWrongFormatError(/^\d{3}$/, cvc, cvcError, "wrong format, numbers only", isValid);
-      if(!outcome) {
-        isValid =false;
-      }
+  }
+
+  if (!cvc) {
+    setBlankError("can't be blank", cvcError);
+    isValid = false;
+    cvcInput.style.border = "1px solid red";
+  }
+
+  if (cvc) {
+    setWrongFormatError(/^\d{3}$/, cvc, cvcError, "wrong format, numbers only", isValid);
+    if (!outcome) {
+      isValid = false;
+      cvcInput.style.border = "1px solid red";
     }
+  }
 
   return isValid;
 
@@ -144,7 +162,7 @@ function validation(cardName, cardNumber, expiryMonth, expiryYear, cvc) {
 
 //function to display error message if the input fields are empty
 function setBlankError(errorMessage, element) {
-	element.innerHTML = `<p> ${errorMessage} </p>`
+  element.innerHTML = `<p> ${errorMessage} </p>`
 }
 
 //function to display error message if the input values are in wrong format
@@ -152,10 +170,10 @@ function setWrongFormatError(value, inputValue, element, errorMessage) {
 
   const regex = new RegExp(value);
 
-	if(!regex.test(inputValue)) {
-		element.innerHTML  = `<p> ${errorMessage} </p>`;
+  if (!regex.test(inputValue)) {
+    element.innerHTML = `<p> ${errorMessage} </p>`;
     outcome = false;
-  }else {
+  } else {
     outcome = true;
   }
 
@@ -165,9 +183,9 @@ function setWrongFormatError(value, inputValue, element, errorMessage) {
 
 //function to redirect to next page
 function redirect() {
-	if(secondDivComputedStyle.display === 'none') {
-		secondPage.style.display = "inline-flex";
-		formData.style.display = "none";
+  if (secondDivComputedStyle.display === 'none') {
+    secondPage.style.display = "inline-flex";
+    formData.style.display = "none";
   }
 }
 
